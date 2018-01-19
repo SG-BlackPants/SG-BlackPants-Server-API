b8d2276d768c671aac68505d07df24d74cada7d7
loloara <addy94@naver.com>
2018-01-19 17:11:52 +0900

    add article route

--- config/express_config.js
+++ config/express_config.js
@@ -21,6 +21,7 @@ module.exports = function(){
 
   require('../app/routes/users.server.routes.js')(app);
   require('../app/routes/keywords.server.routes.js')(app);
+  require('../app/routes/articles.server.routes.js')(app);
   app.use(express.static('./public'));
 
   return app;
