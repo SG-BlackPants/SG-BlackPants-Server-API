57a31564ec79c7583b9499ce0d3075a68c63db06
loloara <addy94@naver.com>
2018-01-23 17:12:27 +0900

    change database name

--- config/env/development.js
+++ config/env/development.js
@@ -1,6 +1,6 @@
 module.exports = {
-  // mongodb : 'mongodb://localhost/univScanner',
-  mongodb : 'mongodb://ec2-52-23-164-26.compute-1.amazonaws.com/univScanner',
+  // mongodb : 'mongodb://localhost/univscanner',
+  mongodb : 'mongodb://ec2-52-23-164-26.compute-1.amazonaws.com/univscanner',
   elasticsearch : 'localhost:9200',
   algorithm : 'aes-256-cbc',
   key : 'thisissecretkey'
