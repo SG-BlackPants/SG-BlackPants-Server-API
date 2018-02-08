060b3c5606845b14be47301c2387732e37e32594
loloara <addy94@naver.com>
2018-02-08 18:06:14 +0900

    be making mail auth system

--- app/apis/nodemailer.js
+++ app/apis/nodemailer.js
@@ -1,5 +1,6 @@
 const nodemailer = require('nodemailer'),
-      account = require('../../serviceAccountKey');
+      account = require('../../serviceAccountKey'),
+      redis = require('./redis');
 
 const transporter = nodemailer.createTransport({
   service: "google",
@@ -13,7 +14,21 @@ module.exports = user => {
   const mailOption = {
     from : account.google_email,
     to : user.email,
-    subject : "test",
-    text : "링크를 클릭하시면 인증이 완료됩니다. http://ec2-52-23-164-26.compute-1.amazonaws.com:3000/firebase/" + user._id + "/email/"
+    subject : "UnivScanner 가입 인증메일입니다",
+    text : "링크를 클릭하시면 인증이 완료됩니다. http://ec2-52-23-164-26.compute-1.amazonaws.com:3000/firebase/" + user._id + "/email/verify"
   };
+
+
+
+  transporter.sendMail(mailOption, (err, info) => {
+    if(err) return next(err);
+
+
+
+    res.json({
+      "result" : "SUCCESS",
+      "code" : "SEND_MAIL",
+      "message" : "success to send mail (" + user.email + ")"
+    })
+  });
 };
--- app/routes/firebase.server.routes.js
+++ app/routes/firebase.server.routes.js
@@ -7,8 +7,11 @@ module.exports = app => {
   app.route('/firebase/new')
     .post(fb.findKeywordsForPush, fb.findUserByKeywordAndPush);
 
-  app.route('/firebase/:userId/email/reqauth')
+  app.route('/firebase/:userId/email/request')
     .get(firebase.sendEmailForAuth);
+
+  app.route('/firebase/:userId/email/verify')
+    .get(firebase.verifyEmail);
 
   app.route('/firebase/:userId/email/checkauth')
     .get(firebase.checkEmailAuth);
